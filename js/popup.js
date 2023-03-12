fd6c30619a9845ec2d2a6b540a725c970c8a6d1f
Admin <site@mail.com>
2023-03-12 17:04:32 +0300

    Added popup.js file with close popup function

---
+++ js/popup.js
@@ -0,0 +1,13 @@
+$('document').ready(function() {
+
+	$('.popupHead a').click(function(e) {
+		e.preventDefault();
+		$('.popup').css("display", "none");
+	});
+
+	$('.popupHead a').click(function(e) {
+		e.preventDefault();
+		$('.popup').css("display", "none");
+	});
+
+});
